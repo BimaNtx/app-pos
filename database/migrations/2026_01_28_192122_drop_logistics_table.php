df248d2319a5a76e2f0d9a5de74b4233fdeb3ae7
Maybe-Adelardd <axsuroplayzyt@gmail.com>
2026-01-28 19:26:33 +0700

    delete: logistics page

--- database/migrations/2026_01_28_192122_drop_logistics_table.php
+++ database/migrations/2026_01_28_192122_drop_logistics_table.php
@@ -4,12 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
+    {
+        Schema::dropIfExists('logistics');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::create('logistics', function (Blueprint $table) {
             $table->id();
@@ -19,13 +26,5 @@ return new class extends Migration
             $table->integer('minimum_stock')->default(10);
             $table->timestamps();
         });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('logistics');
     }
 };
